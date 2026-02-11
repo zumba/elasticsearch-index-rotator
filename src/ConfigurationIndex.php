aaa4383449a1ec694c3658ec9c1349e9db53776f
jcvincenti <juancruzvincenti@gmail.com>
2026-02-11 15:06:20 -0300

    REQ-19986: Make index rotator ES 6.8 compatible

--- src/ConfigurationIndex.php
+++ src/ConfigurationIndex.php
@@ -35,7 +35,7 @@ class ConfigurationIndex
 		'mappings' => [
 			'configuration' => [
 				'properties' => [
-					'name' => ['type' => 'string', 'index' => 'not_analyzed'],
+					'name' => ['type' => 'keyword'],
 					'timestamp' => ['type' => 'date']
 				]
 			]
